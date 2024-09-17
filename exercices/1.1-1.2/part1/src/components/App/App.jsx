b99a2403db7837e1cb9dcce86fccc38a43240401
Alexis <alexis.cattaruzza@student.vinci.be>
2024-09-17 15:48:13 +0200

    1.1-1.2 : CSS style

--- exercices/1.1-1.2/part1/src/components/App/App.jsx
+++ exercices/1.1-1.2/part1/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import Header from '../Header/Header';
 import Content from '../Content/Content';
 import Total from '../Total/Total';
+import './App.css';
 
 const App = () => {
   const course = 'Half Stack application development';
