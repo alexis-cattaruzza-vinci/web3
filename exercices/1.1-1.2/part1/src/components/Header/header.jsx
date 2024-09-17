6deae261211264351f47c2dc591bcc2275bb6d15
Alexis <alexis.cattaruzza@student.vinci.be>
2024-09-17 16:00:35 +0200

    1.1-1.2 : import image

--- exercices/1.1-1.2/part1/src/components/Header/header.jsx
+++ exercices/1.1-1.2/part1/src/components/Header/header.jsx
@@ -1,5 +1,12 @@
+import logo from './LOGOHEVINCI.jpeg';
+
 const Header = ({ course }) => {
-    return <h1>{course}</h1>;
-  };
+  return (
+    <div>
+      <h1>{course}</h1>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+};
 
 export default Header;
