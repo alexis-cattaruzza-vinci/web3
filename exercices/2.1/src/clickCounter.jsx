16d9fe96b9790b1e2b9227a8ee053746af3596ac
Alexis <alexis.cattaruzza@student.vinci.be>
2024-09-24 14:21:10 +0200

    new:ex2.1-destructuring

--- exercices/2.1/src/clickCounter.jsx
+++ exercices/2.1/src/clickCounter.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 
-const ClickCounter = () => {
+const ClickCounter = ({title, message}) => {
 
     const [count, setCount] = useState(0);
 
     return (
         <div className="card">
+        <p>
+            {title}
+        </p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {count >= 10 && <p>{message}</p>}
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/clickCounter.jsx</code> and save to test HMR
         </p>
       </div>
     );
