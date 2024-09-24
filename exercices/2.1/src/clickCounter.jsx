6128fb08fc32aa104111ec94966e3b389b2bb685
Alexis <alexis.cattaruzza@student.vinci.be>
2024-09-24 14:32:53 +0200

    new:ex2.1-events

--- exercices/2.1/src/clickCounter.jsx
+++ exercices/2.1/src/clickCounter.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 
-const ClickCounter = ({title, message}) => {
+const ClickCounter = ({title, message, messageMouse}) => {
 
     const [count, setCount] = useState(0);
+    const [showMessage, setShowMessage] = useState(false);
 
     return (
         <div className="card">
         <p>
             {title}
         </p>
-        <button onClick={() => setCount((count) => count + 1)}>
+        {showMessage && <p>{messageMouse}</p>}
+        <button onClick={() => setCount((count) => count + 1)} 
+        onMouseEnter={() => setShowMessage(true)} 
+        onMouseLeave={() => setShowMessage(false)}>
           count is {count}
         </button>
         {count >= 10 && <p>{message}</p>}
